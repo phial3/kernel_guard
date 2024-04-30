c7bebd23ec5c09b5cfcc8b27dcd3d17fb840129b
郑友捷 <Azure_stars@126.com>
2024-04-30 16:49:42 +0800

    feat: split the aarch64
    1. split the aarch64 to arch_boot
    2. fix the doc missing

--- src/lib.rs
+++ src/lib.rs
@@ -4,11 +4,6 @@
 //! The critical section is created after the guard struct is created, and is
 //! ended when the guard falls out of scope.
 //!
-//! The crate user must implement the [`KernelGuardIf`] trait using
-//! [`crate_interface::impl_interface`] to provide the low-level implementantion
-//! of how to enable/disable kernel preemption, if the feature `preempt` is
-//! enabled.
-//!
 //! Available guards:
 //!
 //! - [`NoOp`]: Does nothing around the critical section.
@@ -18,30 +13,9 @@
 //! - [`NoPreemptIrqSave`]: Disables/enables both kernel preemption and local
 //!   IRQs around the critical section.
 //!
-//! # Crate features
-//!
-//! - `preempt`: Use in the preemptive system. If this feature is enabled, you
-//!    need to implement the [`KernelGuardIf`] trait in other crates. Otherwise
-//!    the preemption enable/disable operations will be no-ops. This feature is
-//!    disabled by default.
-//!
 //! # Examples
 //!
 //! ```
-//! use kernel_guard::{KernelGuardIf, NoPreempt};
-//!
-//! struct KernelGuardIfImpl;
-//!
-//! #[crate_interface::impl_interface]
-//! impl KernelGuardIf for KernelGuardIfImpl {
-//!     fn enable_preempt() {
-//!         // Your implementation here
-//!     }
-//!     fn disable_preempt() {
-//!         // Your implementation here
-//!     }
-//! }
-//!
 //! let guard = NoPreempt::new();
 //! /* The critical section starts here
 //!
